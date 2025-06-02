7ed1154f9800dde257b30fbf0fa53a2eb8cb313b
amolvkharche <83961171+amolvkharche@users.noreply.github.com>
2025-06-02 15:55:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,9 +12,7 @@ helm repo update
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.1/cert-manager.yaml
 
-helm install ingress-nginx ingress-nginx/ingress-nginx \
-  --namespace ingress-nginx \
-  --create-namespace
+helm upgrade --install ingress-nginx ingress-nginx   --repo https://kubernetes.github.io/ingress-nginx   --namespace ingress-nginx --create-namespace
 
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller  --timeout=60s
 
